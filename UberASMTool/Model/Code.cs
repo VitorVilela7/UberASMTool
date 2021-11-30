e216f417626fd94af005bf5c746558ba453a998c
Vitor Vilela <vitor.vilela.mario@gmail.com>
2021-11-29 22:22:54 -0300

    Basic refactor

--- UberASMTool/Model/Code.cs
+++ UberASMTool/Model/Code.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace UberASMTool
+namespace UberASMTool.Model
 {
 	class Code
 	{
--- UberASMTool/Model/UberConfig.cs
+++ UberASMTool/Model/UberConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UberASMTool.Model;
 
 namespace UberASMTool
 {
